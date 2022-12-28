31a48e8e978e4c30bd66fb1a6789a47305298934
Long Nguyen <longn@zillowgroup.com>
2022-12-28 03:55:36 -0500

    docker post install: migrate data-root dir

--- late-commands/install-docker.sh
+++ late-commands/install-docker.sh
@@ -2,6 +2,7 @@
 # docs https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
 
 USER="${1:-hadoop}"
+DATA_ROOT="${2:-/home/universal/docker/data-root}"
 
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
@@ -24,10 +25,26 @@ echo '--------------------------------------------------------------------------
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 echo '----------------------------------------------------------------------------'
 
+# set Docker data-root dir to a different path not affected by OS re-installation
+# https://tienbm90.medium.com/how-to-change-docker-root-data-directory-89a39be1a70b
+service docker stop
+touch /etc/docker/daemon.json
+cat > /etc/docker/daemon.json << 'EOF'
+{
+   "data-root": "data_root_value"
+}
+EOF
+mkdir -p "$DATA_ROOT"
+sed -i "s%data_root_value%$DATA_ROOT%g" /etc/docker/daemon.json
+rsync -aP /var/lib/docker/ "$DATA_ROOT"
+mv /var/lib/docker /var/lib/docker.old
+service docker start
+echo '----------------------------------------------------------------------------'
+
 # add user $USER to docker group to streamline usage
 # https://docs.docker.com/engine/install/linux-postinstall/
 groupadd docker
 usermod -aG docker $USER
 
-sudo -H -u hadoop docker run hello-world
+sudo -H -u $USER docker run hello-world
 echo '----------------------------------------------------------------------------'
