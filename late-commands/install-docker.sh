818a5a27e315bc33895a2b26a62042883e6f849b
Long Nguyen <longn@zillowgroup.com>
2022-12-27 20:47:24 -0500

    apt-get install no prompt

--- late-commands/install-docker.sh
+++ late-commands/install-docker.sh
@@ -3,18 +3,24 @@
 
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-apt-get install ca-certificates curl gnupg lsb-release
+apt-get install -y ca-certificates curl gnupg lsb-release
+echo '----------------------------------------------------------------------------'
 
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# overwrite /etc/apt/keyrings/docker.gpg if already existed
+# https://stackoverflow.com/questions/7519375/how-to-automatically-overwrite-the-output-file-when-running-gpg-i-e-without
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
+echo '----------------------------------------------------------------------------'
 
 # simply install the latest version for now
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo '----------------------------------------------------------------------------'
 
 docker run hello-world
+echo '----------------------------------------------------------------------------'
