a245af4f39ca7cfe9895b040b7cfc26196b8aff4
Long Nguyen <longn@zillowgroup.com>
2023-01-13 18:41:01 -0500

    little update

--- late-commands/server-misc.sh
+++ late-commands/server-misc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+USER="${1:-hadoop}"
+
 # disable server from going to sleep https://www.unixtutorial.org/disable-sleep-on-ubuntu-server/
 systemctl mask sleep.target suspend.target hibernate.target
 systemctl status sleep.target
 
+# allow $USER to own "universal" files
+chown -R $USER:$USER /home/universal
 
