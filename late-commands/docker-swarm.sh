d4ec513fb8f3ce892e836a6985c0688ad9d1b01f
Long Nguyen <longn@zillowgroup.com>
2023-01-15 18:38:28 -0500

    docker swarm: set correct node labels for control-tower

--- late-commands/docker-swarm.sh
+++ late-commands/docker-swarm.sh
@@ -7,6 +7,9 @@ MASTER_IP=$(hostname -I | awk '{print $1}')
 echo "MASTER_IP=$MASTER_IP"
 
 sudo -H -u $USER docker swarm init --advertise-addr $MASTER_IP
+# Set control-tower node labels - current settings: amd64/linux, has NVIDIA GPUs driver installed w/ cuda11.7
+sudo -H -u $USER docker node update --label-add arch=amd64 --label-add os=linux --label-add gpu=gtx-1080-g1 --label-add cuda=11.7  $(hostname)
+# Inspect control-tower
 sudo -H -u $USER docker node inspect self --pretty
 
 
